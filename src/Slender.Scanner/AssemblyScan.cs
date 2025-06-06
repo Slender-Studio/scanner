e2526838aed5ba38a0422960ae5040fe9834bff7
James Roberts <James@Robertscom.com>
2025-06-06 20:35:07 +1000

    Guard against null parameters
    #4

--- src/Slender.Scanner/AssemblyScan.cs
+++ src/Slender.Scanner/AssemblyScan.cs
@@ -34,8 +34,17 @@ namespace Slender.Scanner
         /// </summary>
         /// <param name="assemblies">The assemblies to get the types from.</param>
         /// <returns>Itself.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="assemblies"/> is null.</exception>
         public AssemblyScan AddAssemblies(IEnumerable<Assembly> assemblies)
-            => this.AddAssemblyScan(new AssemblyScan { Types = assemblies.SelectMany(a => a.GetTypes()).ToArray() });
+            => this.AddAssemblyScan(
+                new AssemblyScan
+                {
+                    Types
+                        = (assemblies ?? throw new ArgumentNullException(nameof(assemblies)))
+                            .Where(a => a != null)
+                            .SelectMany(a => a.GetTypes())
+                            .ToArray()
+                });
 
         /// <summary>
         /// Adds the types from the specified assemblies to this <see cref="AssemblyScan"/>.
@@ -43,25 +52,31 @@ namespace Slender.Scanner
         /// <param name="assembly">An <see cref="Assembly"/> to get types from.</param>
         /// <param name="additionalAssemblies">Additional assemblies to get types from.</param>
         /// <returns>Itself.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="assembly"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="additionalAssemblies"/> is null.</exception>
         public AssemblyScan AddAssemblies(Assembly assembly, params Assembly[] additionalAssemblies)
-            => this.AddAssemblies(new[] { assembly }.Union(additionalAssemblies));
+            => this.AddAssemblies(
+                new[] { assembly ?? throw new ArgumentNullException(nameof(assembly)) }
+                    .Union(additionalAssemblies ?? throw new ArgumentNullException(nameof(additionalAssemblies))));
 
         /// <summary>
         /// Adds the types from the specified <paramref name="assembly"/> to this <see cref="AssemblyScan"/>.
         /// </summary>
         /// <param name="assembly">The <see cref="Assembly"/> to get types from.</param>
         /// <returns>Itself.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="assembly"/> is null.</exception>
         public AssemblyScan AddAssembly(Assembly assembly)
-            => this.AddAssemblies(assembly);
+            => this.AddAssemblies(assembly ?? throw new ArgumentNullException(nameof(assembly)));
 
         /// <summary>
         /// Adds the types from the specified <paramref name="assemblyScan"/> to this <see cref="AssemblyScan"/>.
         /// </summary>
         /// <param name="assemblyScan">The <see cref="AssemblyScan"/> to get types from.</param>
         /// <returns>Itself.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="assemblyScan"/> is null.</exception>
         public AssemblyScan AddAssemblyScan(IAssemblyScan assemblyScan)
         {
-            this.Types = this.Types.Union(assemblyScan.Types).ToArray();
+            this.Types = this.Types.Union((assemblyScan ?? throw new ArgumentNullException(nameof(assemblyScan))).Types).ToArray();
 
             return this;
         }
@@ -78,8 +93,9 @@ namespace Slender.Scanner
         /// </summary>
         /// <param name="assemblies">The assemblies to get the types from.</param>
         /// <returns>A populated <see cref="AssemblyScan"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="assemblies"/> is null.</exception>
         public static AssemblyScan FromAssemblies(IEnumerable<Assembly> assemblies)
-            => new AssemblyScan().AddAssemblies(assemblies);
+            => new AssemblyScan().AddAssemblies(assemblies ?? throw new ArgumentNullException(nameof(assemblies)));
 
         /// <summary>
         /// Gets an <see cref="AssemblyScan"/> with types from the specified assemblies.
@@ -87,16 +103,21 @@ namespace Slender.Scanner
         /// <param name="assembly">An <see cref="Assembly"/> to get types from.</param>
         /// <param name="additionalAssemblies">Additional assemblies to get types from.</param>
         /// <returns>A populated <see cref="AssemblyScan"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="assembly"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="additionalAssemblies"/> is null.</exception>
         public static AssemblyScan FromAssemblies(Assembly assembly, params Assembly[] additionalAssemblies)
-            => new AssemblyScan().AddAssemblies(assembly, additionalAssemblies);
+            => new AssemblyScan().AddAssemblies(
+                assembly ?? throw new ArgumentNullException(nameof(assembly)),
+                additionalAssemblies ?? throw new ArgumentNullException(nameof(additionalAssemblies)));
 
         /// <summary>
         /// Gets an <see cref="AssemblyScan"/> with types from the specified <paramref name="assembly"/>.
         /// </summary>
         /// <param name="assembly">The <see cref="Assembly"/> to get types from.</param>
         /// <returns>A populated <see cref="AssemblyScan"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="assembly"/> is null.</exception>
         public static AssemblyScan FromAssembly(Assembly assembly)
-            => new AssemblyScan().AddAssembly(assembly);
+            => new AssemblyScan().AddAssembly(assembly ?? throw new ArgumentNullException(nameof(assembly)));
 
         #endregion Methods
 
